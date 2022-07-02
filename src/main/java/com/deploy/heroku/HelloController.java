0ee6566cfb48a2cca0493536bff292e56265ae21
Fel-Cas <andres.cmonsalve@gmail.com>
2022-07-02 17:14:22 -0500

    Cambio en la ruta raiz

--- src/main/java/com/deploy/heroku/HelloController.java
+++ src/main/java/com/deploy/heroku/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
                 """;
     }
 
-    @GetMapping("/bootstrap")
+    @GetMapping("/")
     public String bootstrap(){
         return """
                 <!doctype html>
